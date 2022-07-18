daabb20d1cef1126c723dad1e4e7eadf46cb4ca8
Timur <timur.chochiev@gmail.com>
2022-07-18 21:26:27 +0300

    День второй

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import React from "react";
+
+import { Header } from "./components/Header";
+import { Categories } from "./components/Categories";
+import { Sort } from "./components/Sort";
+import { PizzaCard } from "./components/PizzaCard";
+
+import "./scss/app.scss";
+
 function App() {
   return (
-    <div className="App">
-      <h1> New begining! </h1>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaCard title="Мекс" price="500" />
+            <PizzaCard />
+            <PizzaCard />
+            <PizzaCard />
+            <PizzaCard />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
