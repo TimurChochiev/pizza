1f52ee85ff68db6bb7407361b56d588b6fc303aa
Timur <timur.chochiev@gmail.com>
2022-07-22 18:09:02 +0300

    test branch

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import { Header } from "./components/Header";
 import { Categories } from "./components/Categories";
 import { Sort } from "./components/Sort";
 import { PizzaCard } from "./components/PizzaCard";
-
-import pizzas from "./assets/pizzas.json";
+import { useEffect, useState } from "react";
 
 import "./scss/app.scss";
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    try {
+      async function pizzaFetch() {
+        await fetch("https://62b9c7c041bf319d22855607.mockapi.io/pizzas")
+          .then((res) => {
+            return res.json();
+          })
+          .then((res) => {
+            setPizzas(res);
+          });
+      }
+      pizzaFetch();
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
   return (
     <div className="wrapper">
       <Header />
